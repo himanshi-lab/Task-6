878618a1170fbc75d7810bb90b8f2054798b6a68
himanshi-lab <himanshi1237065@gmail.com>
2026-04-20 21:47:39 +0530

    Add files via upload

---
+++ task6.sql
@@ -0,0 +1,8 @@
+SELECT 
+    EXTRACT(YEAR FROM order_date) AS year,
+    EXTRACT(MONTH FROM order_date) AS month,
+    SUM(amount) AS total_revenue,
+    COUNT(DISTINCT order_id) AS order_volume
+FROM online_sales
+GROUP BY year, month
+ORDER BY year, month;
